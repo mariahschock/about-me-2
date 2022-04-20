b25d18fbe804a9ee25e2e35b8be5b271a1ae8a51
Mariah <mariahschock@gmail.com>
2022-04-20 16:39:06 -0700

    JS work

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 // import functions
 
 // grab DOM elements
-
+const btn = document.getElementById('btn');
+const animalDiv = document.getElementById('animalDiv');
 // set event listeners 
+btn.addEventListener('click', () => {
+    console.log('btn clicked');
+    animalDiv.classList.remove('hidden');
+});
+
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state
+    
