28adb533e268c8306ab5827cb0bb4b2143d15bed
mariahschock <100049077+mariahschock@users.noreply.github.com>
2022-04-20 17:03:00 -0700

    Revert "Dev"

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 // import functions
 
 // grab DOM elements
-const btn = document.getElementById('btn');
-const animalDiv = document.getElementById('animalDiv');
-// set event listeners 
-btn.addEventListener('click', () => {
-    animalDiv.classList.toggle('hidden');
-});
 
+// set event listeners 
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state
-    
